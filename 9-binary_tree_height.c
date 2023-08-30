768be72a5a9624120816a32f4b12bef7f27296e2
Ardiy <you@example.com>
2023-08-30 21:08:39 +0000

    height of binary tree fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,17 +1,32 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_postorder - traverses tree postorderly
+ * @tree: pointer to the root node of the tree to traverse
+ * @funct: pointer to a function that calls each node
+ * return: none
+ */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+    size_t h1;
+    size_t h2;
 
-	if (tree == NULL)
-		return (0);
-	
-	if (tree->right == NULL && tree->left == NULL)
-		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+    if (!tree)
+    {
+        return (0);
+    }
 
-	return (left_height > right_height ? left_height + 1: right_height + 1);
+    h1 = binary_tree_height(tree->left);
+    h2 = binary_tree_height(tree->right);
+
+    if (h1 > h2)
+    {
+        return (h1 + 1);
+    }
+    else
+    {
+        return (h2 + 1);
+    }
+
+    return (0);
 }
