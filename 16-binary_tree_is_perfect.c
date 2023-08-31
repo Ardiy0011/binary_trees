7a616bf7f8295e4e6b916619b214cb2dfefeb9e8
Ardiy <you@example.com>
2023-08-31 23:04:43 +0000

    perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,37 +7,65 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (tree == NULL)
+    int rec;
+
+    if (!tree)
     {
-        return 0;
+        return (0);
     }
 
-    int left_height = binary_tree_height(tree->left);
-    int right_height = binary_tree_height(tree->right);
-
-    if (left_height == right_height)
-    {
-        return binary_tree_is_perfect(tree->left) &&
-               binary_tree_is_perfect(tree->right);
-    }
-
-    return 0;
+    size_t depth = binary_tree_depth(tree);
+    rec = p_rec(tree, depth, 0);
+    return( rec);
 }
 
 /**
- * binary_tree_height - compute the height of a binary tree
+ * binary_tree_depth - compute the depth of a binary tree
  * @tree: pointer to the root node of the tree
- * Return: height of the tree
+ * Return: depth of the tree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL)
+    size_t h1;
+
+    if (!tree)
+    {
+        return (0);
+    }
+
+	if (!tree->parent)
+    {
+        return (0);
+    }
+
+    h1 = binary_tree_depth(tree->parent) + 1;
+    return (h1);
+}
+
+/**
+ * p_rec - check if a tree is perfect
+ * @tree: pointer to the current node in the tree
+ * @depth: depth of the tree
+ * @level: current level in the tree
+ * Return: 1 if perfect, 0 otherwise
+ */
+int p_rec(const binary_tree_t *tree, size_t depth, size_t level)
+{
+    if (!tree)
+    {
+        return (1);
+    }
+
+    if (!tree->left && !tree->right)
+    {
+        return depth == level + 1;
+    }
+
+    if (!tree->left || !tree->right)
     {
         return 0;
     }
 
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
-
-    return (left_height > right_height ? left_height : right_height) + 1;
+    return p_rec(tree->left, depth, level + 1) &&
+           p_rec(tree->right, depth, level + 1);
 }
