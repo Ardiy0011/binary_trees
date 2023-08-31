695e4098030698409480c6c650c5d18dcff12033
Ardiy <you@example.com>
2023-08-31 22:40:56 +0000

    full tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,32 +8,26 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-
-    int scenario1 = tree->left && tree->right;
-    int scenario2 = !tree->left && !tree->right;
-    int scenario3 = !tree->left || !tree->right;
-
-
-    if (tree == NULL)
+    int scenario1 = !tree;
+    int scenario2 = tree->left && tree->right;
+    int scenario3 = !tree->left && !tree->right;
+    
+    if (scenario1)
     {
         return (1);
-    }
-
-    if (scenario3)
-    {
-        return (0);
     }
 
     if (scenario2)
-    {
-        return (1);
-    }
-
-    if (scenario1)
     {
         int traverse_lft = binary_tree_is_full(tree->left);
         int traverse_rgt = binary_tree_is_full(tree->right);
         return (traverse_lft && traverse_rgt);
     }
+    
+    if (scenario3)
+    {
+        return (1);
+    }
+
     return (0);
 }
