96c17378177de9bb9dee06d072d9dec1b3cdeddf
Ardiy <you@example.com>
2023-08-31 22:52:14 +0000

    full tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,32 +2,31 @@
 /**
  * binary_tree_is_full - validate if tree is full
  * @tree: pointer to the root node of the tree to traverse
- * @funct: pointer to a function that calls each node
  * return: 0
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    int scenario1 = !tree;
-    int scenario2 = tree->left && tree->right;
-    int scenario3 = !tree->left && !tree->right;
-    
-    if (scenario1)
-    {
-        return (1);
-    }
+	int scenario1 = !tree;
+	int scenario2 = tree->left && tree->right;
+	int scenario3 = !tree->left && !tree->right;
 
-    if (scenario2)
-    {
-        int traverse_lft = binary_tree_is_full(tree->left);
-        int traverse_rgt = binary_tree_is_full(tree->right);
-        return (traverse_lft && traverse_rgt);
-    }
-    
-    if (scenario3)
-    {
-        return (1);
-    }
+	if (scenario1)
+	{
+	return (1);
+	}
 
-    return (0);
+	if (scenario2)
+	{
+	int traverse_lft = binary_tree_is_full(tree->left);
+	int traverse_rgt = binary_tree_is_full(tree->right);
+	return (traverse_lft && traverse_rgt);
+	}
+
+	if (scenario3)
+	{
+	return (1);
+	}
+
+	return (0);
 }
