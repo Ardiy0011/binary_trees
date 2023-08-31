aef9ef3bc58f4dcfb62780327b7af775fbd578de
Ardiy <you@example.com>
2023-08-31 01:42:16 +0000

    binary balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,12 +1,10 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - traverses tree postorderly
- * @tree: pointer to the root node of the tree to traverse
- * @funct: pointer to a function that calls each node
- * return: none
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure
+ * Return: balance factor of the tree, or 0 if tree is NULL
  */
-
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
     size_t h1;
     size_t h2;
