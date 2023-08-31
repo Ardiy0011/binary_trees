3dc0671cdc472c5f0e9d6eae8198557fa4c55f4a
Ardiy <you@example.com>
2023-08-31 23:39:29 +0000

    perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,6 +23,40 @@ void binary_tree_perhelp(const binary_tree_t *tree, int *leaves)
     binary_tree_perhelp(tree->right, leaves);
 }
 
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - traverses tree postorderly
+ * @tree: pointer to the root node of the tree to traverse
+ * @funct: pointer to a function that calls each node
+ * return: none
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t h1;
+    size_t h2;
+
+    if (!tree)
+    {
+        return (0);
+    }
+
+	if (!tree->right && !tree->left)
+		return (0);
+
+    h1 = binary_tree_height(tree->left);
+    h2 = binary_tree_height(tree->right);
+
+    if (h1 > h2)
+    {
+        return (h1 + 1);
+    }
+    else
+    {
+        return (h2 + 1);
+    }
+
+}
 
 
 /**
