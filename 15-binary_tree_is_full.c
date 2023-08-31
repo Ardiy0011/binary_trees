7310489614d6a483b28a68a5722566133776d3d1
Ardiy <you@example.com>
2023-08-31 13:32:27 +0000

    full tree

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - traverses tree postorderly
+ * @tree: pointer to the root node of the tree to traverse
+ * @funct: pointer to a function that calls each node
+ * return: none
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+
+    int scenario1 = tree->left && tree->right;
+    int scenario2 = !tree->left && !tree->right;
+
+    if (!tree)
+    {
+        return (0);
+    }
+
+    if (scenario1 || scenario2)
+    {
+        return (1);
+    }
+
+}
