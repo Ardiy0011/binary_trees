04241f35ccfe67917ff25a955d3b9b4ec22f5eee
Ardiy <you@example.com>
2023-08-31 00:16:00 +0000

    nodes with only one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,16 +10,16 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
     size_t traverse_left;
     size_t traverse_right;
-    size_t scenatio1 = (tree->left && !tree->right);
-    size_t scenatio2 = (!tree->left && tree->right);
-    size_t scenatio3 = (tree->left && tree->right);
+    size_t scenario1 = (tree->left && !tree->right);
+    size_t scenario2 = (!tree->left && tree->right);
+
 
     if (!tree)
     {
         return (0);
     }
 
-    if (scenatio1 || scenatio2 && scenatio3)
+    if (scenario1 || scenario2)
     {
         return (1);
     }
