9d92b8855bc2af20cbf9f17a10224376bec7d87a
Ardiy <you@example.com>
2023-08-31 00:27:41 +0000

    nodes with only one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,36 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - counts the number of nodes with one child
+ * binary_tree_nodes - counts the number of nodes with at least one child
  * @tree: pointer to the root node of the tree
- * Return: number of nodes with one child in the tree
+ * Return: number of nodes with at least one child in the tree
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t traverse_left;
-    size_t traverse_right;
-    size_t scenario1 = (tree->left && !tree->right);
-    size_t scenario2 = (!tree->left && tree->right);
-    size_t scenario3 = (!tree->left && !tree->right);
-
+    size_t count_left;
+    size_t count_right;
 
     if (!tree)
     {
         return (0);
     }
 
-    if (scenario3)
+    if (tree->left || tree->right)
     {
-        return (1);
+        count_left = binary_tree_nodes(tree->left);
+        count_right = binary_tree_nodes(tree->right);
+        return (count_left + count_right + 1);
     }
 
-    if (scenario1 || scenario2)
-    {
-
-    traverse_left = binary_tree_nodes(tree->left);
-    traverse_right = binary_tree_nodes(tree->right);
-    return (traverse_left + traverse_right);
-    }
     return (0);
 }
