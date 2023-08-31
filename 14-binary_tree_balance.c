22a0c153fc0410ccd086830491f545567f8b588f
Ardiy <you@example.com>
2023-08-31 01:41:10 +0000

    binary balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,21 +30,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
     {
         return (h2 + 1);
     }
-
-}
-
-/**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure
- * Return: balance factor of the tree, or 0 if tree is NULL
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-    if (!tree)
-        return (0);
-
-    int h1 = binary_tree_height(tree->left);
-    int h2 = binary_tree_height(tree->right);
-
     return (h1 - h2);
 }
