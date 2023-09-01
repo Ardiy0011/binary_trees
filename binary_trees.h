c85316d782c8030996f53b5f232d9ccd2ceaba22
Ardiy <you@example.com>
2023-09-01 19:19:56 +0000

    bug fix

--- binary_trees.h
+++ binary_trees.h
@@ -46,5 +46,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_subtree_height(const binary_tree_t *subtree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int binary_subtree_balance(const binary_tree_t *subtree);
 
 #endif /* _BINARY_TREES_H_ */
