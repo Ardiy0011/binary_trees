6a7503cf5b56a4634fb5124b7718e5b221f7b520
Ardiy <you@example.com>
2023-09-01 02:55:01 +0000

    bug fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,9 +3,12 @@
 /**
  * binary_tree_balance - Returns the balance
  * factor of a binary tree.
+ *
  * @tree: A pointer to the root node or subtree
  * in the binary tree.
+ *
  * Return: The balance factor of the tree.
+ *
  **/
 int binary_tree_balance(const binary_tree_t *tree)
 {
@@ -16,15 +19,33 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 
+	/**
+	 * A tree with balance factor of 0 means
+	 * both left subtrees and right subtrees
+	 * have the same height.
+	 *
+	 * A tree with a positive balance factor
+	 * means the left subtree has a higher
+	 * height than the right subtree.
+	 *
+	 * And finally, a negative balance factor
+	 * means the right subtree has a higher height
+	 * than the left subtree.
+	 **/
 	return (binary_subtree_height(subtree->left) -
 		binary_subtree_height(subtree->right));
 }
 
 /**
- * binary_subtree_height - Returns the height of a tree
+ * binary_subtree_height - Returns the height
+ * of subtree for @subtree. @subtree could
+ * also be the root node in the binary tree.
+ *
  * @subtree: A pointer to the subtree whose height
  * subtree needs to be determined.
+ *
  * Return: The height of the subtree.
+ *
  **/
 int binary_subtree_height(const binary_tree_t *subtree)
 {
