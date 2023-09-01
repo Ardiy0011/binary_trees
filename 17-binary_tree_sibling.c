b5eb7dc21823aa23676b0f7bbe4ecb07de2acc08
Ardiy <you@example.com>
2023-09-01 03:00:08 +0000

    bug fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_sibling - Finds the sibling of a node
- * @node: Pointer to the node to find the sibling
- * Return: Pointer to the sibling node, or NULL else found
- */
+ * binary_tree_sibling - Find the sibling of @node.
+ *
+ * @node: A pointer to the node whose sibling is
+ * needed.
+ *
+ * Return: The sibling node
+ *
+ **/
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
-	return (NULL);
+	{
+		return (NULL);
+	}
 
-	binary_tree_t *parent = node->parent;
-
-	if (parent->left == node)
-	return (parent->right);
-
-	else if (parent->right == node)
-	return (parent->left);
-
-	return (NULL);
+	return (node->parent->left == node ?
+		node->parent->right : node->parent->left);
 }
